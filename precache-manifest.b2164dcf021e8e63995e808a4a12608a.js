6f4add3b5543d4066383df20e1db6878fdb919f6
vmarquet <vincent.marquet1@free.fr>
2020-06-24 21:55:27 +0200

    Updates

---
+++ precache-manifest.b2164dcf021e8e63995e808a4a12608a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b405663f7baaa71f19216cf3058c0f18",
+    "url": "/subtitle-corrector/index.html"
+  },
+  {
+    "revision": "b61d790be161b204ff43",
+    "url": "/subtitle-corrector/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "59c0726c38e6d4de85bc",
+    "url": "/subtitle-corrector/static/js/2.48a1a2aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/subtitle-corrector/static/js/2.48a1a2aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61d790be161b204ff43",
+    "url": "/subtitle-corrector/static/js/main.2483fdb5.chunk.js"
+  },
+  {
+    "revision": "e9d234030c74bf646603",
+    "url": "/subtitle-corrector/static/js/runtime-main.08958dd1.js"
+  }
+]);
